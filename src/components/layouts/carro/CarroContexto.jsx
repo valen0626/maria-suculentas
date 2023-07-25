1a53ce77e2d1bd88c75b3ed94a2cf0508bf400a5
jacobososa <stsv1999@hotmail.com>
2023-07-25 12:27:11 -0500

    funcion crear subtotal carro

--- src/components/layouts/carro/CarroContexto.jsx
+++ src/components/layouts/carro/CarroContexto.jsx
@@ -5,29 +5,40 @@ export const CarroContexto = createContext();
 export const CarroProvider = ({ children }) => {
   const [carroItems, setCarroItems] = useState([]);
 
-  const calculateSubtotal = (productoItem) => {
-    return productoItem.precio * productoItem.cantidad;
+  const total = () => {
+    return carroItems.reduce(
+      (total, item) => total + calculateSubtotal(item),
+      0
+    );
   };
 
-  const total = ()=>{
-    return carroItems.reduce((total, item) => total + calculateSubtotal(item), 0);
-  }
   const añadirAlCarro = (productoItem) => {
-    const estaEnCarro = carroItems.some((item) => item.nombre === productoItem.nombre);
+    const estaEnCarro = carroItems.some(
+      (item) => item.nombre === productoItem.nombre
+    );
     if (!estaEnCarro) {
-        setCarroItems([...carroItems,{ ...productoItem, cantidad: 1 }]);
-      } else {
-        console.log("El producto ya está en el carro de compras.");
-      }
+      setCarroItems([...carroItems, productoItem]);
+    } else {
+      console.log("El producto ya está en el carro de compras.");
+    }
   };
 
   const eliminarProducto = (productNombre) => {
-    const updatedCarroItems = carroItems.filter((item) => item.nombre !== productNombre);
+    const updatedCarroItems = carroItems.filter(
+      (item) => item.nombre !== productNombre
+    );
     setCarroItems(updatedCarroItems);
   };
 
   return (
-    <CarroContexto.Provider value={{ total, carroItems, añadirAlCarro, eliminarProducto }}>
+    <CarroContexto.Provider
+      value={{
+        total,
+        carroItems,
+        añadirAlCarro,
+        eliminarProducto,
+      }}
+    >
       {children}
     </CarroContexto.Provider>
   );
--- src/components/layouts/carro/VistaCarro.jsx
+++ src/components/layouts/carro/VistaCarro.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import Header from "../../helpers/Header";
 import { CarroContexto } from "./CarroContexto";
 import { useContext } from "react";
 
 const VistaCarro = () => {
-  const { total, carroItems, eliminarProducto } = useContext(CarroContexto);
+  const {
+    total,
+    carroItems,
+    eliminarProducto,
+  } = useContext(CarroContexto);
+
+  const [cantidad, setCantidad] = useState(1);
+  const [subtotal, setSubtotal] = useState(0)
+
+  // const calculateSubtotal = (precio, cantidad) => {
+  //   return parseInt(precio) * parseInt(cantidad);
+  // };
 
   return (
     <section>
@@ -24,20 +36,35 @@ const VistaCarro = () => {
               </tr>
             </thead>
             <tbody>
-                {carroItems.map((item) => (
-                  <tr key={item.nombre}>
-                    <td><input type="button" value="Eliminar" onClick={() => eliminarProducto(item.nombre)} /></td>
-                    <td><img src={item.imagen} alt="" /></td>
-                    <td>{item.nombre}</td>
-                    <td>{item.precio}</td>
-                    <td>1</td>
-                    <td>{item.precio}</td>
-                  </tr>
-                ))}
+              {carroItems.map((item) => (
+                <tr key={item.nombre}>
+                  <td>
+                    <input
+                      type="button"
+                      value="Eliminar"
+                      onClick={() => eliminarProducto(item.nombre)}
+                    />
+                  </td>
+                  <td>
+                    <img src={item.imagen} alt="" />
+                  </td>
+                  <td>{item.nombre}</td>
+                  <td>{item.precio}</td>
+                  <td>
+                    <input
+                      type="number"
+                      placeholder="Cantidad"
+                      value={cantidad}
+                      onChange={(e) => setCantidad(item.nombre, e.target.value)}
+                    ></input>
+                  </td>
+                  <td>
+                    {setSubtotal(cantidad*item.precio)}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
-          <h1>${total}</h1>
-                  
+          <h1>${subtotal}</h1>
         </section>
       </section>
     </section>
